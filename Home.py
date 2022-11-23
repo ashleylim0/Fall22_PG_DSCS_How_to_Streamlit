acbd624c2318ab9d78ffde45805a4479a67db0b8
k k <118927354+ashleylim0@users.noreply.github.com>
2022-11-23 19:18:36 +0000

    Home.py

--- Home.py
+++ Home.py
@@ -1,17 +1,126 @@
 import streamlit as st
 
-col1, col2 = st.columns(2)
-col2.image('./home/profile.png')
+st.header("Introduction")
 
-col1.header('Marc-Robin Grüner')
-col1.markdown('''
-Hello everyone,
+st.markdown('''
+This is a quick introduction of this project.
+In this project, I am trying to predict Apple, Inc. stock prices based on historic data using machine learning.
 
-My name is Marc and I love working on data science projects.
-On this webiste, I share some of my recent journeys.\n
-You can find them on the left!
-
-In case you find any of these interesting, feel free to reach out:
-
-[LinkedIn](https://www.linkedin.com/in/marc-robin-gruener/)    [Email](mailto:marc-robin.gruener@student.unisg.ch)
+Here is a quick overview of the dataset:
 ''')
+
+# Adapted from: https://www.kaggle.com/code/ryanholbrook/forecasting-with-machine-learning/tutorial
+import pandas as pd
+from pathlib import Path
+
+import plotly.graph_objects as go
+from plotly.colors import n_colors
+
+from sklearn.linear_model import LinearRegression, MultiTaskLasso
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
+from xgboost import XGBRegressor
+from joblib import dump, load
+
+apple_stock = pd.read_csv("./finance/apple.csv", index_col='Date')
+apple_stock.index = pd.to_datetime(apple_stock.index)
+apple_stock.sort_index(inplace=True)
+with st.expander('Raw Data'):
+  st.dataframe(apple_stock)
+
+st.header("Method")
+st.markdown('''
+We want to predict the price of the next 7 days on the last 30 days' closing price.
+''')
+
+
+def make_lags(ts, lags, lead_time=1):
+  return pd.concat(
+    {f'y_lag_{i}': ts.shift(i)
+     for i in range(lead_time, lags + lead_time)},
+    axis=1)
+
+
+# Thirty days of lag features
+y = apple_stock.Close.copy()
+X = make_lags(y, lags=30).fillna(0.0)
+with st.expander('Manipulated Data'):
+  st.dataframe(X)
+
+st.markdown('''
+I am doing a standard train-test split which is non-shuffled with test being 25%
+''')
+
+code = '''X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, shuffle=False)'''
+st.code(code, language='python')
+
+tab1, tab2, tab3 = st.tabs(['LinReg', 'XGBoost', 'New Model'])
+
+
+def create_visualization(y_test: pd.DataFrame,
+                         y_pred: pd.DataFrame) -> go.Figure:
+  fig = go.Figure()
+
+  colorscale = n_colors('rgb(67, 198, 172)',
+                        'rgb(25, 22, 84)',
+                        len(y_pred),
+                        colortype='rgb')
+
+  for i, (index, row) in enumerate(y_pred.iterrows()):
+    fig.add_trace(
+      go.Scatter(x=pd.period_range(start=index,
+                                   periods=len(row)).to_timestamp(),
+                 y=row,
+                 line=dict(color=colorscale[i])))
+  fig.add_trace(
+    go.Scatter(x=y_test.index, y=y_test.y_step_1, line=dict(color='black')))
+  fig.update_layout(showlegend=False)
+
+  return fig
+
+
+apple_stock = pd.read_csv("./finance/apple.csv", index_col='Date')
+apple_stock.index = pd.to_datetime(apple_stock.index)
+apple_stock.sort_index(inplace=True)
+
+
+def make_multistep_target(ts, steps):
+  return pd.concat({f'y_step_{i + 1}': ts.shift(-i)
+                    for i in range(steps)},
+                   axis=1)
+
+
+# 7 Day forecast
+y = make_multistep_target(y, steps=7).dropna()
+
+# Shifting has created indexes that don't match. Only keep times for
+# which we have both targets and features.
+y, X = y.align(X, join='inner', axis=0)
+
+# Create splits
+X_train, X_test, y_train, y_test = train_test_split(X,
+                                                    y,
+                                                    test_size=0.25,
+                                                    shuffle=False)
+
+with tab1:
+  st.subheader('Linear Regression')
+  ### Model 1: Basic Direct LinReg
+  model = LinearRegression()
+  model.fit(X_train, y_train)
+  dump(model, './finance/dir_linreg.joblib')
+
+  y_fit = pd.DataFrame(model.predict(X_train),
+                       index=X_train.index,
+                       columns=y.columns)
+  y_pred = pd.DataFrame(model.predict(X_test),
+                        index=X_test.index,
+                        columns=y.columns)
+
+  train_rmse = mean_squared_error(y_train, y_fit, squared=False)
+  test_rmse = mean_squared_error(y_test, y_pred, squared=False)
+  print((f"Train RMSE: {train_rmse:.2f}\n"
+         f"Test RMSE: {test_rmse:.2f}"))
+
+  st.plotly_chart(create_visualization(y_train, y_fit), use_container_width=True)
+  st.plotly_chart(create_visualization(y_test, y_pred), use_container_width=True)
--- pages/2_Heart_Attack_Predictions.py
+++ pages/2_Heart_Attack_Predictions.py
@@ -4,7 +4,6 @@ from joblib import load
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 
-
 st.title('Prediction Machines Workshop')
 
 knn = load('./heart_attack/knn.joblib')
@@ -15,63 +14,85 @@ svc = load('./heart_attack/svc.joblib')
 col1, col2, col3 = st.columns(3)
 
 with col1:
-    age = st.slider('Age in years', 29, 77)
-    trtbps = st.slider('Resting Blood Pressure', 94, 200)
-    restecg_values = {'normal':0 , 'ST-T wave abnormality': 1, 'ventricular hypertrophy': 2}
-    restecg = restecg_values[st.selectbox('Esting Electrocardiographic Results', ('normal', 'ST-T wave abnormality', 'ventricular hypertrophy'))]
-    oldpeak = st.slider('ST depression', 0.0, 6.2, 0.0, 0.1)
-    thall_values = {'normal': 0, 'fixed defect': 1, 'reversable defect': 2}
-    thall = thall_values[st.selectbox('Thall', ('normal', 'fixed defect', 'reversable defect'))]
+  age = st.slider('Age in years', 29, 77)
+  trtbps = st.slider('Resting Blood Pressure', 94, 200)
+  restecg_values = {
+    'normal': 0,
+    'ST-T wave abnormality': 1,
+    'ventricular hypertrophy': 2
+  }
+  restecg = restecg_values[st.selectbox(
+    'Esting Electrocardiographic Results',
+    ('normal', 'ST-T wave abnormality', 'ventricular hypertrophy'))]
+  oldpeak = st.slider('ST depression', 0.0, 6.2, 0.0, 0.1)
+  thall_values = {'normal': 0, 'fixed defect': 1, 'reversable defect': 2}
+  thall = thall_values[st.selectbox(
+    'Thall', ('normal', 'fixed defect', 'reversable defect'))]
 
 with col2:
-    sex = 1 if st.radio('Sex', ('male', 'female')) == 'male' else 0
-    chol = st.slider('Serum Cholestoral', 126, 564)
-    thalachh = st.slider('Maximum Heart Rate', 71, 202)
-    slp_values = {'upsloping': 0, 'flat': 1, 'downsloping': 2}
-    slp = slp_values[st.selectbox('Slope', ('upsloping', 'flat', 'downsloping'))]
+  sex = 1 if st.radio('Sex', ('male', 'female')) == 'male' else 0
+  chol = st.slider('Serum Cholestoral', 126, 564)
+  thalachh = st.slider('Maximum Heart Rate', 71, 202)
+  slp_values = {'upsloping': 0, 'flat': 1, 'downsloping': 2}
+  slp = slp_values[st.selectbox('Slope', ('upsloping', 'flat', 'downsloping'))]
 
 with col3:
-    cp_values = {'Typical angina': 0, 'Atypical angina': 1, 'Non-anginal pain': 2, 'Asympomatic': 3}
-    cp = cp_values[st.selectbox('Chest Pain Type',('Typical angina', 'Atypical angina', 'Non-anginal pain', 'Asympomatic'))]
-    fbps = 1 if st.radio('Fasting Blood Pressure', ("> 120 mg/dL", "<= 120 mg/dL")) == "> 120 mg/dL" else 0
-    exng = 1 if st.radio('Exercise Induced Angina',('yes', 'no')) == 'yes' else 0
-    caa = st.slider('Number of major Vessels colored', 0, 4, 0, 1)
-
+  cp_values = {
+    'Typical angina': 0,
+    'Atypical angina': 1,
+    'Non-anginal pain': 2,
+    'Asympomatic': 3
+  }
+  cp = cp_values[st.selectbox(
+    'Chest Pain Type',
+    ('Typical angina', 'Atypical angina', 'Non-anginal pain', 'Asympomatic'))]
+  fbps = 1 if st.radio('Fasting Blood Pressure',
+                       ("> 120 mg/dL", "<= 120 mg/dL")) == "> 120 mg/dL" else 0
+  exng = 1 if st.radio('Exercise Induced Angina',
+                       ('yes', 'no')) == 'yes' else 0
+  caa = st.slider('Number of major Vessels colored', 0, 4, 0, 1)
 
 st.header('Predictions')
 
-
 data = pd.read_csv("./heart_attack/heart.csv")
-
 """Only 14 attributes used:
 13. #51 (thal):  3 = normal; 6 = fixed defect; 7 = reversable defect
 """
 
 
 def preprocess_inputs(df):
-    df = df.copy()
-    
-    # Split X and y: X being all of the explanatory variables, y being the outcome to predict 
-    X = df.drop('output', axis=1)
-    y = df['output']
-    
-    # Train and test sets: These are four 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8, shuffle=True, random_state=0)
-    
-    X_copy = X_train
-    # Scale X with a standard scaler: This scales the data so that we can use it more easily for seperation
-    scaler = StandardScaler()
-    X_train = pd.DataFrame(scaler.fit_transform(X_train), index=X_train.index, columns=X_train.columns)
-    X_test = pd.DataFrame(scaler.transform(X_test), index=X_test.index, columns=X_test.columns)
-    
-    return X_train, X_test, y_train, y_test, X_copy
+  df = df.copy()
+
+  # Split X and y: X being all of the explanatory variables, y being the outcome to predict
+  X = df.drop('output', axis=1)
+  y = df['output']
+
+  # Train and test sets: These are four
+  X_train, X_test, y_train, y_test = train_test_split(X,
+                                                      y,
+                                                      train_size=0.8,
+                                                      shuffle=True,
+                                                      random_state=0)
+
+  X_copy = X_train
+  # Scale X with a standard scaler: This scales the data so that we can use it more easily for seperation
+  scaler = StandardScaler()
+  X_train = pd.DataFrame(scaler.fit_transform(X_train),
+                         index=X_train.index,
+                         columns=X_train.columns)
+  X_test = pd.DataFrame(scaler.transform(X_test),
+                        index=X_test.index,
+                        columns=X_test.columns)
+
+  return X_train, X_test, y_train, y_test, X_copy
+
 
 X_train, X_test, y_train, y_test, X_copy = preprocess_inputs(data)
 
+input_list = [[age], [sex], [cp], [trtbps], [chol], [fbps], [restecg],
+              [thalachh], [exng], [oldpeak], [slp], [caa], [thall]]
 
-input_list = [[age], [sex], [cp], [trtbps], [chol],[fbps], [restecg], [thalachh], [exng], [oldpeak], [slp], [caa], [thall] ]
-
-#Make a dict: 
+#Make a dict:
 dct = {}
 
 for i in range(0, len(X_train.columns)):
@@ -81,11 +102,12 @@ for i in range(0, len(X_train.columns)):
 input = pd.DataFrame.from_dict(dct)
 input_manipulated = X_copy.append(input)
 
-input_manipulated = input_manipulated.reset_index(drop = True)
+input_manipulated = input_manipulated.reset_index(drop=True)
 
 scaler = StandardScaler()
 
-x_input = pd.DataFrame(scaler.fit_transform(input_manipulated), columns=input_manipulated.columns)
+x_input = pd.DataFrame(scaler.fit_transform(input_manipulated),
+                       columns=input_manipulated.columns)
 
 pred_knn = knn.predict(x_input)
 pred_log = logistic.predict(x_input)
@@ -98,4 +120,5 @@ col5.metric('Log', pred_log[242])
 col6.metric('RF', pred_rf[242])
 col7.metric('SVC', pred_svc[242])
 
-st.markdown('''[Source](https://github.com/TechClubHSG/Fall22_PG_DSCS_How_to_ML.git)''')
+st.markdown(
+  '''[Source](https://github.com/TechClubHSG/Fall22_PG_DSCS_How_to_ML.git)''')
